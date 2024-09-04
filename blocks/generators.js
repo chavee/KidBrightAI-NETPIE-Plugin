b1ddaca47241c2056f4ad11e44a3ef7ece3014c4
chavee <i@chavee.com>
2024-09-04 18:23:57 +0700

    initial commit

---
+++ blocks/generators.js
@@ -0,0 +1,268 @@
+let cb_netpie_on_connected_count = 0;
+let cb_netpie_on_disconnected_count = 0;
+let cb_netpie_on_reveived_msg_count = 0;
+let cb_netpie_read_shadow_count = 0;
+let cb_netpie_on_shadow_updated_count = 0;
+
+function objectDotToBracket(path) {
+    return path.split('.').map(item => `['${item}']`).join('');
+}
+
+function objectDotToJSON(path, val) {
+    let out = {};
+    let value;
+    let a = path.split('.');
+    for (let i=a.length-1; i>=0; i--) {
+      if (value === undefined) {
+        out[a[i]] = val;
+      }
+      else {
+        out[a[i]] = {...value};
+      }
+      value = out;
+      out = {}
+    }
+    return value;
+}
+
+function globalVariableDeclaration(block, generator) {
+    let globals = [];
+    let workspace = block.workspace;
+    let variables = Blockly.Variables.allUsedVarModels(workspace) || [];
+    for (let i = 0, variable; variable = variables[i]; i++) {
+      globals.push(variable.name);
+    }
+    return globals.length ?
+        generator.INDENT + 'global ' + globals.join(', ') : '';
+}
+
+function globalCodeDeclaration(block, generator) {
+    generator.definitions_['import_enums'] = 'import enums';
+    generator.definitions_['import_matcher'] = 'import matcher';
+    generator.definitions_['import_packettypes'] = 'import packettypes';
+    generator.definitions_['import_properties'] = 'import properties';
+    generator.definitions_['import_publish'] = 'import publish';
+    generator.definitions_['import_reasoncodes'] = 'import reasoncodes';
+    generator.definitions_['import_subscribe'] = 'import subscribe';
+    generator.definitions_['import_subscribeoptions'] = 'import subscribeoptions';
+    generator.definitions_['import_mqttclient'] = 'import mqttclient as MQTT';
+    generator.definitions_['import_netpie'] = 'import netpie as NETPIE';
+
+    generator.definitions_['new_microgear'] = 'microgear = NETPIE.Microgear()';
+}
+
+python.pythonGenerator.forBlock['netpie_connect'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let device_id = generator.valueToCode(block, 'device_id', python.Order.ATOMIC) || '';
+    let device_token = generator.valueToCode(block, 'device_token', python.Order.ATOMIC) || '';
+    let sub_private_msg = block.getFieldValue('sub_private_msg').toLowerCase() == 'true';
+    let sub_shadow_updated = block.getFieldValue('sub_shadow_updated').toLowerCase() == 'true';
+    
+    let code = `microgear.setDeviceCredential(${device_id}, ${device_token})\n`;
+    if (sub_private_msg) code += `microgear.subscribe('@private/#')\n`;
+    if (sub_shadow_updated) code += `microgear.subscribe('@shadow/data/updated')\n`;
+    code += `microgear.connect()\n`;
+
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_publish'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let subtopic = block.getFieldValue('topic');
+    let topic = `'@msg/${subtopic}'`;
+    let payload = generator.valueToCode(block, 'payload', python.Order.ATOMIC) || '';
+    let code = `microgear.publish(${topic},${payload})\n`
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_on_connected'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let statements_callback = generator.statementToCode(block, 'callback') + '  pass';
+    let functionName = generator.provideFunction_(
+      'cb_netpie_on_connected_'+(cb_netpie_on_connected_count++),
+      ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '():',
+      globalVariableDeclaration(block, generator),
+      statements_callback]
+    );
+
+    let code = `microgear.on('Connected', ${functionName})\n`;
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_on_disconnected'] = function(block, generator) {
+  globalCodeDeclaration(block, generator);
+
+  let statements_callback = generator.statementToCode(block, 'callback') + '  pass';  
+  let functionName = generator.provideFunction_(
+    'cb_netpie_on_disconnected_'+(cb_netpie_on_disconnected_count++),
+    ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '():',
+    globalVariableDeclaration(block, generator),
+    statements_callback]
+  );
+
+  let code = `microgear.on('Disconnected', ${functionName})\n`;
+  return code;
+};
+
+python.pythonGenerator.forBlock['netpie_subscribe'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let subtopic = block.getFieldValue('topic');
+    let topic = `'@msg/${subtopic}'`;
+    let code = `microgear.subscribe(${topic})\n`
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_on_reveived_msg'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let subtopic = block.getFieldValue('topic');
+    let topic = `'@msg/${subtopic}'`;
+    let statements_callback = generator.statementToCode(block, 'callback') + '  pass';
+    let functionName = generator.provideFunction_(
+      'cb_netpie_on_reveived_msg_'+(cb_netpie_on_reveived_msg_count++),
+      ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '(topic, payload):',
+      globalVariableDeclaration(block, generator),
+      statements_callback]
+    );
+
+    let code = `microgear.on(${topic}, ${functionName})\n`;
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_msg_payload'] = function(block, generator) {
+  let datatype = block.getFieldValue('datatype');
+
+  let code;
+  switch (datatype) {
+    case 'string' :
+            code = `payload.decode('utf-8')`;
+            break;
+    case 'int' :
+            code = `int(payload)`;
+            break;
+    case 'float' :
+            code = `float(payload)`;
+            break;
+    case 'bool' :
+            code = `bool(payload)`;
+            break;
+  }
+  return [code, python.Order.NONE]; 
+};
+
+python.pythonGenerator.forBlock['netpie_write_shadow_field'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let field = generator.valueToCode(block, 'field', python.Order.ATOMIC) || '';
+    let value = generator.valueToCode(block, 'value', python.Order.ATOMIC) || '';
+    let code = `microgear.writeShadowField(${field}, ${value})\n`;
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_read_shadow'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let statements_callback = generator.statementToCode(block, 'callback') + '  pass';
+    let functionName = generator.provideFunction_(
+      'cb_netpie_read_shadow_'+(cb_netpie_read_shadow_count++),
+      ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '(shadow):',
+      globalVariableDeclaration(block, generator),
+      statements_callback]
+    );
+
+    let code = `microgear.getShadowData(${functionName})\n`;
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_on_shadow_updated'] = function(block, generator) {
+    globalCodeDeclaration(block, generator);
+
+    let statements_callback = generator.statementToCode(block, 'callback') + '  pass';
+    let functionName = generator.provideFunction_(
+      'cb_netpie_on_shadow_updated_'+(cb_netpie_on_shadow_updated_count++),
+      ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '(shadow):',
+      globalVariableDeclaration(block, generator),
+      statements_callback]
+    );
+
+    let code = `microgear.on('ShadowUpdated', ${functionName})\n`;
+    return code;
+};
+
+python.pythonGenerator.forBlock['netpie_shadow_field'] = function(block, generator) {
+  let field = generator.valueToCode(block, 'field', python.Order.ATOMIC) || '';
+  let datatype = block.getFieldValue('datatype');
+  let code;
+
+  let obj = 'shadow'+objectDotToBracket(field.replace(/'/g,""));
+  switch (datatype) {
+    case 'string' :
+            code = `str(${obj})`;
+            break;
+    case 'int' :
+            code = `int(${obj})`;
+            break;
+    case 'float' :
+            code = `float(${obj})`;
+            break;
+  }
+  return [code, python.Order.NONE]; 
+};
+
+python.pythonGenerator.forBlock['netpie_on_reveived_private_msg'] = function(block, generator) {
+  globalCodeDeclaration(block, generator);
+
+  let subtopic = block.getFieldValue('topic');
+  let topic = `'@private/${subtopic}'`;
+
+  let statements_callback = generator.statementToCode(block, 'callback') + '  pass';
+  let functionName = generator.provideFunction_(
+    'cb_netpie_on_reveived_msg_'+(cb_netpie_on_reveived_msg_count++),
+    ['def ' + generator.FUNCTION_NAME_PLACEHOLDER_ + '(topic, payload):',
+    globalVariableDeclaration(block, generator),
+    statements_callback]
+  );
+
+  let code = `microgear.on(${topic}, ${functionName})\n`;
+  return code;
+};
+
+python.pythonGenerator.forBlock['netpie_private_msg_payload'] = function(block, generator) {
+  let datatype = block.getFieldValue('datatype');
+
+  let code;
+  switch (datatype) {
+    case 'string' :
+            code = `payload.decode('utf-8')`;
+            break;
+    case 'int' :
+            code = `int(payload)`;
+            break;
+    case 'float' :
+            code = `float(payload)`;
+            break;
+  }
+  return [code, python.Order.NONE]; 
+};
+
+python.pythonGenerator.forBlock['netpie_text'] = function(block, generator) {
+  let value = block.getFieldValue('value');
+  let code = `"${String(value)}"`;
+  return [code, python.Order.NONE]; 
+};
+
+python.pythonGenerator.forBlock['netpie_number'] = function(block, generator) {
+  let value = block.getFieldValue('value');
+  let code = `${value}`;
+  return [code, python.Order.NONE]; 
+};
+
+python.pythonGenerator.forBlock['netpie_boolean'] = function(block, generator) {
+  let value = block.getFieldValue('value');
+  let code = `${value}`;
+  return [code, python.Order.NONE]; 
+};
---
+++ libs/netpie.py
@@ -0,0 +1,167 @@
+import json
+import threading
+import mqttclient as MQTT
+
+MIN_DELAY = 1
+MAX_DELAY = 15
+
+MQTT_ADDRESS = "mqtt.netpie.io"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 30
+
+MQTT_STATE_DISCONNECTED  = 0
+MQTT_STATE_CONNECTING    = 1
+MQTT_STATE_CONNECTED     = 2
+
+class Microgear:
+    def mg_message_cb(self, client, userdata, msg):
+        topic = msg.topic
+        payload = msg.payload.decode()
+        if topic.startswith('@msg/'):
+            for tc in self.topic_callbacks:
+                if self.topicMatched(tc[0],topic):
+                    tc[1](topic, payload)
+        elif topic == '@shadow/data/updated':
+            try:
+                sobj = json.loads(payload)
+                for sc in self.shadowupdated_callbacks:
+                    sc(sobj['data'])
+            except KeyError as e:
+                    pass
+        elif topic == '@private/shadow/data/get/response':
+            sobj = json.loads(payload)
+            for sgc in self.shadowget_callbacks:
+                sgc(sobj)
+            self.shadowget_callbacks.clear()
+
+    def mg_connected_cb(self, client, userdata, flags, rc):
+        print('rc', rc)
+        if rc == 0:
+            self.mqtt_state = MQTT_STATE_CONNECTED
+
+            for t in self.topic_subscriptions:
+                self.mqttclient.subscribe(t)
+            for c in self.connected_callbacks:
+                c()
+        else:
+            pass
+
+    def mg_disconnected_cb(self):
+        self.mqtt_state = MQTT_STATE_DISCONNECTED
+        for c in self.disconnected_callbacks:
+            c()
+
+    def __init__(self, deviceid=None, devicetoken=None):
+
+        self.mqtt_state = MQTT_STATE_DISCONNECTED
+        self.deviceid = deviceid
+        self.devicetoken = devicetoken
+        self.connected_callbacks = []
+        self.topic_callbacks = []
+        self.disconnected_callbacks = []
+        self.shadowupdated_callbacks = []
+        self.topic_subscriptions = []
+        self.shadowget_callbacks = []
+
+    def setDeviceCredential(self, deviceid, devicetoken):
+        self.deviceid = deviceid
+        self.devicetoken = devicetoken
+
+    def on(self, event, cb):
+        if   event == 'Message' :
+            self.topic_callbacks.append(['@msg/#', cb])
+        elif event == 'ShadowUpdated' :
+            self.shadowupdated_callbacks.append(cb)
+        elif event == 'Connected' :
+            self.connected_callbacks.append(cb)
+        elif event == 'Disconnected' :
+            self.disconnected_callbacks.append(cb)
+        elif event.startswith('@msg/'):
+            self.topic_callbacks.append([event, cb])
+        elif event == '@shadow/data/updated':
+            self.shadowupdated_callbacks.append(cb)
+        elif event.startswith('@private/'):
+            self.topic_callbacks.append([event, cb])
+
+    def subscribe(self, topic):
+        if topic not in self.topic_subscriptions:
+            self.topic_subscriptions.append(topic)
+        if self.mqtt_state == MQTT_STATE_CONNECTED:
+            self.mqttclient.subscribe(topic)
+
+    def check_msg(self):
+        if (self.mqtt_state) == MQTT_STATE_CONNECTED:
+            return self.mqttclient.check_msg()
+        elif (self.mqtt_state == MQTT_STATE_DISCONNECTED):
+            print("reconnected...")
+            self.mqttclient.connect()
+
+    def mqtttask(self):
+        self.mqttclient.loop_forever()
+
+    def connect(self):
+        if bool(self.deviceid) and bool(self.devicetoken) and self.mqtt_state==MQTT_STATE_DISCONNECTED :
+            self.mqtt_state = MQTT_STATE_CONNECTING
+            self.mqttclient = MQTT.Client(client_id=self.deviceid)
+            self.mqttclient.username_pw_set(self.devicetoken, '')
+            self.mqttclient.on_connect = self.mg_connected_cb
+            self.mqttclient.on_message = self.mg_message_cb
+            self.mqttclient.reconnect_delay_set(min_delay=MIN_DELAY, max_delay=MAX_DELAY)
+            self.mqttclient.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_KEEPALIVE)
+            mqtt_thread_instance = threading.Thread(target=self.mqtttask, args=())
+            mqtt_thread_instance.start()
+
+    def getShadowData(self, cb):
+        if self.mqtt_state == MQTT_STATE_CONNECTED:
+            self.shadowget_callbacks.append(cb)
+            self.mqttclient.publish('@shadow/data/get','')
+
+    def publish(self, topic, payload):
+        if self.mqtt_state == MQTT_STATE_CONNECTED:
+            self.mqttclient.publish(topic, payload)
+
+    def writeShadowField(self, field, value):
+        def covertDotNotationToJSON(path, val):
+            value = None
+            out = {}
+            a = path.split('.')
+            for i in range(len(a)-1, -1, -1) :
+                if value is None :
+                    out[a[i]] = val
+                else:
+                    out[a[i]] = value.copy()
+
+                value = out;
+                out = {}
+            return value;
+        
+        payload = json.dumps({'data': covertDotNotationToJSON(field, value)})
+        if self.mqtt_state == MQTT_STATE_CONNECTED:
+            self.mqttclient.publish('@shadow/data/update', payload)
+        else:
+            self.connect()
+
+    def topicMatched(self, sub, topic):
+        s = 0
+        t = 0
+        while s<len(sub):
+            if sub[s] == '#':
+                if s+1 == len(sub): return 1
+                else: return 0
+            elif sub[s] == '+':
+                if t == len(topic) or (s+1<len(sub) and sub[s+1]!='/'): return 0
+            else:
+                while s<len(sub) and sub[s]!='/' and t<len(topic) and topic[t]!='/':
+                    if sub[s]!=topic[t]: return 0
+                    s = s+1
+                    t = t+1
+                if (s<len(sub) and sub[s]!='/') or (t<len(topic) and topic[t]!='/'): return 0
+
+            while s<len(sub) and sub[s]!='/': s=s+1
+            while t<len(topic) and topic[t]!='/':t=t+1
+
+            if s<len(sub) and sub[s]=='/': s = s+1
+            if t<len(topic) and topic[t]=='/': t = t+1
+
+        if t == len(topic): return 1
+        else: return 0
